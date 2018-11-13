318e5b15c3ed4288916058ad27cc723895b45937
Tony To <tony.to@fundingcircle.com>
2018-11-13 11:02:27 +0000

    echoing progress in the install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ MY_PATH="`( cd \"$MY_PATH\" && pwd  )`"  # absolutized and normalized
 if [ -z "$MY_PATH"  ] ; then
 exit 1  # fail
 fi
+echo "-- symlinking tmux.conf"
 ln -sf "${MY_PATH}/tmux.conf" ~/.tmux.conf
+echo "-- symlinking vimrc"
 ln -sf "${MY_PATH}/vim/vimrc" ~/.vimrc
+echo "-- symlinking zshrc"
 ln -sf "${MY_PATH}/zshrc" ~/.zshrc
+
+echo "-- install complete --"
